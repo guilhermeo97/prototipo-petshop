a414dace571e5aa480c8c056515aa0bc8c1e649d
guilhermeo97 <oliveiraguilherme591@gmail.com>
2024-03-20 20:40:56 -0300

    banner criado com temporizador

--- Assets/scripts/script.js
+++ Assets/scripts/script.js
@@ -1,3 +1,13 @@
 function alertaAgendamento(){
     console.log("Agendamento realizado com sucesso!")
 }
+
+var vetor_banner = ["/Assets/imagens/img-banner-4.png", "/Assets/imagens/img-banner-5.png", "/Assets/imagens/img-banner-6.png"];
+var posicao_vetor = 0;
+
+function func_banner(){
+    posicao_vetor = (posicao_vetor + 1) % 3;
+    document.querySelector("#banner").src = vetor_banner[posicao_vetor]
+}
+
+setInterval(func_banner, 3000) 

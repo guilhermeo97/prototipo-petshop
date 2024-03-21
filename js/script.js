3c8fede86b9b8e7d2dc1ee068c79c45e028da415
guilhermeo97 <55219644+guilhermeo97@users.noreply.github.com>
2024-03-20 23:48:52 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ function alertaAgendamento(){
     console.log("Agendamento realizado com sucesso!")
 }
 
-var vetor_banner = ["../prototipo-petshop/blob/main/assets/images/img-banner-4.png", "../prototipo-petshop/blob/main/assets/images/img-banner-5.png", "../prototipo-petshop/blob/main/assets/images/img-banner-6.png"];
+var vetor_banner = ["../prototipo-petshop/blob/main/assets/images/img-banner-4.png", "../prototipo-petshop/blob/main/assets/images/img-banner-5.png", "https://github.com/guilhermeo97/prototipo-petshop/blob/main/assets/images/img-banner-6.PNG"];
 var posicao_vetor = 0;
 
 function func_banner(){
